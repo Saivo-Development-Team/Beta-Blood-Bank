dcb27476fa5bea2cf1a22f8fe4d692f5aa95a3a7
aaron <aaronnel.button@gmail.com>
2019-12-02 16:56:32 +0200

    changed login controller it can now accept a employee number and password from the database. Appropriate buttons and help functions added.

--- src/beta/blood/auth/LoginController.java
+++ src/beta/blood/auth/LoginController.java
@@ -6,13 +6,20 @@
 package beta.blood.auth;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
 
 /**
  * FXML Controller class
@@ -22,28 +29,50 @@ import javax.swing.JOptionPane;
 public class LoginController implements Initializable {
     @FXML TextField number;
     @FXML PasswordField password;
+    @FXML Label message;
+                
+    @FXML
+    private void login() throws Exception {
+        
+             Connection conn;
+             Statement s;
+             ResultSet rs;
+    try{
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/betablooddatabase", "root", "");
+            System.out.println("Connected");
             
-    @FXML
-    private void login(){        
-        if("admin".equals(number.getText())){
-                if("admin".equals(password.getText())){
-                    System.out.println("login");
+            s = conn.createStatement();
+            rs = s.executeQuery("SELECT * FROM employee");
+            
+            while(rs.next()){
+                if(rs.getString(1).equals(number.getText())){
+                if(rs.getString(4).equals(password.getText())){
+                    message.setText("Logged in");                    
                 }else{
-                    System.out.println("incorrect password");
+                    message.setText("Incorrect password");
                 }
-            }else{
-                System.out.println("incorrect username or password");
-            }   
-    }
-    
-    @FXML
-    private void exit() {
-        System.exit(0);
+                }else{
+                    message.setText("Incorrect username or password");
+                }   
+            }
+
+    } catch (SQLException ex) {
+            System.out.println("Connection to database failed");
+        }
     }
     
     @FXML
     private void help() {
-        JOptionPane.showMessageDialog(null, "enter");
+        JOptionPane.showMessageDialog(null, "Enter employee number and password provided by your administrator", "Help me", INFORMATION_MESSAGE);
+    }
+    
+    @FXML
+    private void exit() {
+        int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Close Application", OK_CANCEL_OPTION);
+        if(result == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
+        
     }
     
     /**
