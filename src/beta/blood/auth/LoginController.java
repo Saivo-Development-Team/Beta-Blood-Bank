7a9f48afeebb9e8acb4a3c7aebde2be9d3d28c45
Ian Mubangizi <io@Ianmubangizi.com>
2019-12-02 10:39:11 +0200

    UI Testing

---
+++ src/beta/blood/auth/LoginController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beta.blood.auth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author perso
+ */
+public class LoginController implements Initializable {
+    @FXML TextField number;
+    @FXML PasswordField password;
+            
+    @FXML
+    private void login(){
+        System.out.println(number.getText() + " " + password.getText());
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
