d30e8734a106ce70715a0256a1bd1401afc51c9e
Ian Mubangizi <io@Ianmubangizi.com>
2019-12-04 11:59:49 +0200

    Made Use of global Stage object

--- src/beta/blood/nurse/NurseHomeController.java
+++ src/beta/blood/nurse/NurseHomeController.java
@@ -5,18 +5,14 @@
  */
 package beta.blood.nurse;
 
+import beta.blood.Handler;
 import beta.blood.database.DatabaseService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.YES_NO_OPTION;
 
@@ -30,28 +26,21 @@ public class NurseHomeController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
-private void addDonor() {
-    
-}
-    
-   
-@FXML
-private void logout(ActionEvent event) throws IOException{
-    int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to log out", "Logout", YES_NO_OPTION);
-    if(result == JOptionPane.YES_OPTION){
-        Parent nurseHomeParent = FXMLLoader.load(getClass().getResource("/beta/blood/auth/Login.fxml"));
-        Scene nurseHomeScene = new Scene(nurseHomeParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(nurseHomeScene);
-        window.show();
+    private void addDonor() {
+        
     }
-    
-}
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+        int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to log out", "Logout", YES_NO_OPTION);
+        if (result == JOptionPane.YES_OPTION) {
+            Handler.setScene(getClass(), "Login Screen", "/beta/blood/auth/Login.fxml");
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
