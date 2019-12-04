8516ce788b7d303c4bd0e81a2b16880ce1b321d3
Ian Mubangizi <io@Ianmubangizi.com>
2019-12-04 09:24:00 +0200

    check Details, with configured Database

--- src/beta/blood/auth/LoginController.java
+++ src/beta/blood/auth/LoginController.java
@@ -5,23 +5,13 @@
  */
 package beta.blood.auth;
 
-import beta.blood.database.DatabaseService;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -34,71 +24,58 @@ import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
  * @author perso
  */
 public class LoginController implements Initializable {
-    DatabaseService dbService = DatabaseService.service();
-    @FXML TextField number;
-    @FXML PasswordField password;
-    @FXML Label message;
 
-                
+    LoginService loginService = new LoginService();
+    @FXML
+    TextField number;
+    @FXML
+    PasswordField password;
+    @FXML
+    Label message;
+    private int access;
+
     @FXML
     private void login() throws Exception {
-        
-             Connection conn;
-             Statement s;
-             ResultSet rs;
-             PreparedStatement ps;
-    try{
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/betablooddatabase","root","");
-        ps = conn.prepareStatement("SELECT * FROM employee WHERE employeeNumber = ? AND Password = ?");
-        ps.setString(1, number.getText());
-        ps.setString(2, password.getText());
-        rs = ps.executeQuery();
 
-        if(rs.next()){
-                String userType = rs.getString("Admin");
-                if (userType.equals("1")){
-                    System.out.println("Welcome admin");
-                    Parent root2 = FXMLLoader.load(getClass().getResource("Login.fxml"));
-                    Scene sceneAdminHome = new Scene(root2);
-                    //stage.setScene(sceneAdminHome);
-                } else if (userType.equals("0")){
-                    System.out.println("Welcome nurse");
-                }
-        } else{
-            conn.close();
-            JOptionPane.showMessageDialog(null,"UserName Or Password Invalid");
-        }
-            
-        }catch(SQLException ex){
-            System.out.println("Unable to connect");
+        access = loginService.checkDetails(number.getText(), password.getText());
+
+        switch (access) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
         }
     }
-        
+
     @FXML
     private void help() {
         JOptionPane.showMessageDialog(null, "Enter employee number and password provided by your administrator", "Help me", OK_CANCEL_OPTION);
     }
-    
+
     @FXML
     public void openWebpage() {
         try {
             Desktop.getDesktop().browse(new URL("https://sanbs.org.za/").toURI());
-        } catch (IOException | URISyntaxException e) {}
+        } catch (IOException | URISyntaxException e) {
+        }
     }
-    
+
     @FXML
     private void exit() {
         int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Close Application", OK_CANCEL_OPTION);
-        if(result == JOptionPane.YES_OPTION) {
+        if (result == JOptionPane.YES_OPTION) {
             System.exit(0);
         }
-        
+
     }
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
--- src/beta/blood/model/Employee.java
+++ src/beta/blood/model/Employee.java
@@ -5,12 +5,85 @@
  */
 package beta.blood.model;
 
+import beta.blood.auth.LoginService;
+import beta.blood.database.DatabaseService;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Elsa
  */
 public class Employee {
 
-    String employeeId, name, surname, telephone, branch, password;
-    private int position;
+    private final String employeeId, name, surname, telephone, branch, password;
+    private final int position;
+    
+
+    public Employee(String employeeId, String name, String surname,String telephone, String branch, String password, int position) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.surname = surname;
+        this.telephone = telephone;
+        this.branch = branch;
+        this.password = password;
+        this.position = position;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    
+    
+    public static Employee getById(String employeeId) {
+        try {
+            String getQuery = String.format(
+                   "select * from employee where employeeId='%s'", employeeId
+            );
+            ResultSet result = DatabaseService.service().executeQuery(getQuery);
+
+            if (result.next()) {
+                return new Employee(
+                        result.getString("EmployeeId"),
+                        result.getString("Name"),
+                        result.getString("Surname"),
+                        result.getString("Telephone"),
+                        result.getString("Branch"),
+                        result.getString("Password"),
+                        result.getInt("Position")
+                );
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
