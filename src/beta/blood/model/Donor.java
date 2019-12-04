1e84ca77c2fbaa55bec60f4fefa25665f506cd92
Ethan Rinquest <42733768+EthanRinquest@users.noreply.github.com>
2019-12-04 10:31:10 +0200

    added attributes to the entities

--- src/beta/blood/model/Donor.java
+++ src/beta/blood/model/Donor.java
@@ -12,6 +12,46 @@ package beta.blood.model;
 public class Donor {
 
     private int donorID, age, answers;
-    String name, surname, address, gender;
+    private String name, surname, address, gender;
+
+    public Donor(int donorID, int age, int answers, String name, String surname, String address, String gender) {
+        this.donorID = donorID;
+        this.age = age;
+        this.answers = answers;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public int getDonorID() {
+        return donorID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getAnswers() {
+        return answers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    
+    
 
 }
