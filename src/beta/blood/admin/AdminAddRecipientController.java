8768cfadf83ffbc955a05c0d771a53a24a82400f
aaron <aaronnel.button@gmail.com>
2019-12-05 18:40:54 +0200

    Added and add nurse, add recipient and change password for the admin. Added a change password screen for the nurse. Also added a Questionnaire for the nurse to enter donor info.

---
+++ src/beta/blood/admin/AdminAddRecipientController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beta.blood.admin;
+
+import beta.blood.Handler;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class AdminAddRecipientController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private void back() {
+    Handler.setScene(getClass(), "Verify Blood", "/beta/blood/admin/AdminHome.fxml");
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
