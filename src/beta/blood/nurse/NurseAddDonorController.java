50b75f5c743ffe7d57981231f114b247f559382b
Ian Mubangizi <io@ianmubangizi.com>
2020-01-08 10:06:27 +0200

    Merge branch 'temp' of https://github.com/Saivo-Development-Team/Beta-Blood-Bank into temp
    # Conflicts:
    #	nbproject/project.properties
    #	src/beta/blood/Handler.java
    #	src/beta/blood/admin/AdminHome.fxml
    #	src/beta/blood/admin/AdminHomeController.java
    #	src/beta/blood/auth/LoginController.java
    #	src/beta/blood/database/DatabaseService.java
    #	src/beta/blood/model/Employee.java
    #	src/beta/blood/model/Recipient.java
    #	src/beta/blood/nurse/NurseAddDonor.fxml
    #	src/beta/blood/nurse/NurseAddDonorController.java
    #	src/beta/blood/nurse/NurseHome.fxml
    #	src/beta/blood/nurse/NurseHomeController.java

--- src/beta/blood/nurse/NurseAddDonorController.java
+++ src/beta/blood/nurse/NurseAddDonorController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -45,6 +46,10 @@ public class NurseAddDonorController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //cant get scroll pane to show donor questionnaire without an error
+        ToggleGroup toggle = new ToggleGroup();
+        msex.setToggleGroup(toggle);
+        fsex.setToggleGroup(toggle);
+        
         scrollpane.setContent(Handler.loadFxml(getClass(), "DonorQuestionnaire.fxml"));
         combobox.setItems(options);
     }    
